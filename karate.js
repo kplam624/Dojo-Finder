cc11ffafaecc29c943232e0e4e2a5817da9df53b
kramskb1 <69365814+kramskb1@users.noreply.github.com>
2021-01-26 19:59:15 -0500

    Add files via upload

--- karate.js
+++ karate.js
@@ -1,4 +1,18 @@
 var queryUrl = "https://dojos.info/NewJersey/Counties.aspx"
+var myCenter = new google.maps.LatLng(1.303895, 103.831941);
+    function initialize() {
+        var mapProp = {
+            center: myCenter,
+            zoom: 18,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        var marker = new google.maps.Marker({
+            position: myCenter,
+        });
+        marker.setMap(map);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
 d3.json(queryUrl, function(data) {
    createFeatures(data.features);
 });
