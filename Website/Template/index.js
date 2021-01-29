5d5193a5c35ecb2160c67a203e06065218999528
Nehemiah <69742990+Kato883@users.noreply.github.com>
2021-01-28 20:48:23 -0500

    Create index.js

---
+++ Website/Template/index.js
@@ -0,0 +1,12 @@
+
+var tbody = d3.select("tbody");
+
+console.log(data);
+
+data.forEach((weatherReport) => {
+  var row = tbody.append("tr");
+  Object.entries(weatherReport).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
