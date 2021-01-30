fec0fb8bc000d823b5022760fa9aa687a89159c1
unknown <jtslater2@gmail.com>
2021-01-30 08:18:06 -0500

    renamed file

--- sunburst.js
+++ sunburst.js
@@ -1,40 +1,3 @@
-// var data = [
-// {
-//   "type": "sunburst", 
-//   "labels": labels["Strikes", "Punching", "Kicking", "Other Strikes", 
-//             "Grappling", "Throwing", "Joint Lock", "Pinning Tech",
-//             "Boxing", "Wing CHun", "Karate",
-//             "Taekwondo", "Capoeira", "Savate",
-//             "Lethwei", "Muay Thai", "Kung Fu", "Silat", "Kalaripayattu",
-//             "Hapkido", "Judo", "Sumo", "Wrestling", "Aikido",
-//             "Judo","Jujutsu", "Brazillian jiu-jitsu", "Sambo", "Catch Wrestling", 
-//             "Judo", "Wrestling", "Aikido",
-//                                                                     ],
-//   "parents": ["", "Strikes", "Strikes", "Strikes",
-//              "", "Grappling", "Grappling", "Grappling",
-//              "Punching", "Punching", "Punching", 
-//              "Kicking", "Kicking","Kicking", 
-//              "Other Strikes", "Other Strikes","Other Strikes","Other Strikes","Other Strikes",
-//              "Throwing","Throwing","Throwing","Throwing","Throwing",
-//              "Joint Lock","Joint Lock","Joint Lock","Joint Lock","Joint Lock",
-//              "Pinning Tech", "Pinning Tech","Pinning Tech"           
-//             ],
-//   "values":  [10000, 1000, 1000, 1000, 
-//               10000, 1000, 1000, 1000,    
-//               5,5,5,5,
-//               10,10,10,5,
-//               5,5,5,15,
-//               285,223,198,149,
-//               121,119,82,59,
-//               52,44,44,42, ],
-//     "leaf": {"opacity": 0.4},
-//     "marker": {"line": {"width": 2}},
-//     "branchvalues": 'total'
-// }];
-
-// // data2 = sb_data.labels
-// console.log(data2)
-
 var myPlot = document.getElementById('myDiv'),
     hoverInfo = document.getElementById('hoverinfo');
 
@@ -87,7 +50,7 @@ var layout = {
 };
 
 
-Plotly.newPlot('myDiv', data, layout)
+Plotly.newPlot('starburst', data, layout)
 
 
 //console.log(data2)
