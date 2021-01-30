038fafbaf4df308ffb22c95514ab6bc236338b1a
unknown <jtslater2@gmail.com>
2021-01-30 01:48:03 -0500

    text working

--- test_sb_js.js
+++ test_sb_js.js
@@ -35,32 +35,49 @@
 // // data2 = sb_data.labels
 // console.log(data2)
 
-var parents = sb_data.map(sb_data => sb_data.parents);
-console.log(parents);
-
-var labels = sb_data.map(sb_data => sb_data.labels);
-console.log(labels);
-
-var values = sb_data.map(sb_data => sb_data.locals);
-console.log(values);
+var myPlot = document.getElementById('myDiv'),
+    hoverInfo = document.getElementById('hoverinfo');
 
 
+var parent = sb_data.map(sb_data => sb_data.parents);
+// console.log(parents);
 
+var label = sb_data.map(sb_data => sb_data.labels);
+console.log(label);
+
+var value = sb_data.map(sb_data => sb_data.locals);
+// console.log(values);
+
+var st_texts = sb_data.map(sb_data => sb_data.style_info);
+// console.log(st_texts);
 
 var data = [
 {
   "type":"sunburst",
-  "labels":labels,
-  "parents":parents,
-  "values":values,
+  "labels":label,
+  "parents":parent,
+  "values":value,
+  "hovertext":st_texts,
+  "textbelow":st_texts,
+  "hoverinfo":"label+value",
+
    "leaf": {"opacity": 0.4},
    "marker": {"line": {"width": 2}},
    "branchvalues": 'total'
  }];
 
 
-console.log(sb_data)
-console.log(data)
+//  console.log(sb_data)
+// console.log(data)
+
+// var data2 = [
+//   {
+
+// "labels":labels,
+//   "parents":parents,
+//   "values":st_texts,
+// }];
+
 
 
 var layout = {
@@ -72,4 +89,31 @@ var layout = {
 
 Plotly.newPlot('myDiv', data, layout)
 
-myPlot = document.getElementById("myDiv");
+
+//console.log(data2)
+myPlot 
+      .on("plotly_hover", function(data) {
+        //  console.log(data2);
+        var infoText = data.points.map(function(d) {
+          // return d.hovertext;
+          // console.log(d.labels);
+          return (
+            "<b>Style: </b>" +
+            d.label +
+            "<br> <b>Parent: </b>" +
+            d.parent +
+            "<br><b>Description: </b>" +
+             //d.meta +
+            //  "<br><b>DescriptionIBU: </b>" +
+            //  d.customdata +
+            // "<br><br>" +
+            d.hovertext
+                    );
+        });
+        
+        hoverInfo.innerHTML = infoText.join("<br/>");
+      })
+      .on("plotly_unhover", function(data) {
+        hoverInfo.innerHTML = "";
+      });
+  
