15efd10d6e8690a3d43a2fc968f3741d7c1a11c6
unknown <jtslater2@gmail.com>
2021-01-29 22:01:17 -0500

    Sunburst & datafile working

---
+++ data.js
@@ -0,0 +1,122 @@
+var sb_data = [
+        {
+          "labels": "Karate",
+          "locals": 285,
+          "parents": "Punching",
+          "style_info": "Karate is a popular Japanese martial arts that was originally developed on the islands of Okinawa, Japan. It focuses on kata, punches, elbow strikes, knee strikes and kicks. Many Karate schools also conduct Kobudo weapons training (i.e. Bo). There are many sub-styles of Karate. Some of the major Karate styles include Goju-Ryu, Kyokushin, Shito-Ryu, Shorin-Ryu, Shotokan and Wado-Ryu. In addition, during the summer of 2020, Karate will be an Olympic sport for the first time. This Black Belt Wiki section on Karate provides written instructions and/or videos for Karate kata, kumite, kicks, terminology and much more."
+        },
+        {
+          "labels": "Tae Kwon Do",
+          "locals": 223,
+          "parents": "Kicking",
+          "style_info": "Taekwondo is a popular Korean martial arts that is focused primarily on kicks and punches & strikes. This martial arts is best known for its spectacular kicks (i.e. tornado kick and spinning hook kick). Taekwondo is one of the few martial arts that competes at the Olympics. Taekwondo schools generally provide instruction for basic self-defense, sparring, breaking, joint locks, Korean terminology and some grappling techniques. At most Taekwondo schools, students will also learn either WTF Taekwondo forms or ITF Taekwondo patterns."
+        },
+        {
+          "labels": "Kick Boxing",
+          "locals": 198,
+          "parents": "Kicking",
+          "style_info": "As the name implies, Kickboxing is a martial arts style that combines kicks and boxing-style punches. This results in a powerful combination of hand and leg techniques. In American Kickboxing, the use of elbow strikes and knee strikes is usually forbidden (unlike Muay Thai)."
+        },
+        {
+          "labels": "Mixed Martial Arts",
+          "locals": 149,
+          "parents": "Hybrid",
+          "style_info": "Mixed Martial Arts (MMA) is a martial arts system that utilizes a variety of different martial arts techniques (i.e. techniques from Wrestling, Brazilian Jiu-Jitsu, Boxing, Muay Thai, Karate, etc.) in order to defeat an opponent… most famously in the “Octagon”. Most MMA training focuses on combining stand-up fighting skills (i.e. the knee strikes of Muay Thai) with ground fighting techniques (i.e. submission techniques of Brazilian Jiu-Jitsu). The UFC is probably the best known promoter of mixed martial art events on a global basis."
+        },
+        {
+          "labels": "Jiu-Jitsu",
+          "locals": 121,
+          "parents": "Joint lock/Chokeholds/Submisson holds",
+          "style_info": "This ancient Japanese martial arts style has influenced the development of a number of “younger” martial arts such as Judo and Brazilian Jiu-Jitsu. It is also closely related to the Japanese martial arts of Aikijujitsu."
+        },
+        {
+          "labels": "Brazilian Jiu-jitsu",
+          "locals": 119,
+          "parents": "Joint lock/Chokeholds/Submisson holds",
+          "style_info": "Brazilian Jiu-Jitsu (BJJ) is a Brazilian martial arts focused on ground fighting. This Brazilian martial art exploded in popularity when Royce Gracie used this fighting style in order to dominate much larger opponents in mixed martial arts tournaments in the 1990s. This martial arts style focuses mainly on grappling techniques in order to take opponents to the ground and cause submissions via techniques such as a triangle choke. BJJ students also learn how to escape from opponents via techniques such as the elevator sweep."
+        },
+        {
+          "labels": "Judo",
+          "locals": 82,
+          "parents": "Joint lock/Chokeholds/Submisson holds",
+          "style_info": "Judo is a Japanese martial arts focused on grappling, joint locks and throws. Punches and kicks are generally not practiced at Judo schools. Judo was created in Japan in the 1880s and grew out of the even older Japanese martial arts of Jujutsu. According to the International Judo Federation, the history of Judo “is the history of the shift from a martial art to a modern sport”. Moreover, Judo is consider a parent to newer martial arts such as Sambo and Brazilian Jiu-Jitsu."
+        },
+        {
+          "labels": "Muay Thai",
+          "locals": 59,
+          "parents": "Other using Strikes",
+          "style_info": "Muay Thai is a martial arts style from Thailand that is similar to kickboxing but also involves elbow and knee strikes. Muay Thai is also known for its powerful kicking techniques. Many mixed martial arts fighters employ Muay Thai techniques given the effectiveness of this style’s strikes. A number of MMA training routines combine Muay Thai techniques (i.e. elbow strikes and knee strikes) with the grappling techniques of Brazilian Jiu-Jitsu. The most well-known MMA fighter with a Muay Thai background is Anderson Silva."
+        },
+        {
+          "labels": "Hapkido",
+          "locals": 52,
+          "parents": "Throwing",
+          "style_info": "Hapkido is a Korean martial arts focused on punches, kicks, throws and joint locks. Hapkido classes often have some weapons training (i.e. with staffs, canes and swords). Hapkido also emphasizes circular motion, non-resisting movements and control of an opponent. Unlike the Korean martial arts of Taekwondo, Hapkido generally does not use forms & patterns as part of its training."
+        },
+        {
+          "labels": "Tai chi",
+          "locals": 44,
+          "parents": "Hybrid",
+          "style_info": "Tai Chi is often seen as a “gentle” martial arts style because many seniors use its slow movements in order to improve their health & balance and to reduce stress. Nevertheless, Tai Chi also has “harder” sub-styles and variations that include vigorous self-defense techniques."
+        },
+        {
+          "labels": "Kung Fu",
+          "locals": 44,
+          "parents": "Other using Strikes",
+          "style_info": "Kung Fu (also known as Gung Fu) is a Chinese martial arts that focuses primarily on hand & arm strikes and kicking techniques. However, it is hard to generalize about Kung Fu characteristics because there are many different Kung Fu schools and styles (thus you can not “transfer” easily from one Kung Fu school to another school if you move to another city)."
+        },
+        {
+          "labels": "Aikido",
+          "locals": 42,
+          "parents": "Throwing",
+          "style_info": "Aikido is a Japanese martial arts focused on redirecting an attack away from you. This martial arts style involves grabs, strikes, throws, pins and joint locks. Steven Seagal, the movie actor, is a famous student of Aikido. While some Aikido techniques may look odd (i.e. Why do they have a defense against a knife hand strike to the top of the head? No one does that!), these were originally designed to stop a samurai’s sword attack. Moreover, these types of Aikido techniques are useful in order to defend against attackers armed with clubs, sticks, etc."
+        },
+        {
+          "labels": "Strikes",
+          "locals": 809,
+          "parents": "",
+          "style_info": ""
+        },
+        {
+          "labels": "Grappling",
+          "locals": 416,
+          "parents": "",
+          "style_info": ""
+        },
+        {
+          "labels": "Hybrid",
+          "locals": 193,
+          "parents": "",
+          "style_info": ""
+        },
+        {
+          "labels": "Punching",
+          "locals": 285,
+          "parents": "Strikes",
+          "style_info": ""
+        },
+        {
+          "labels": "Kicking",
+          "locals": 421,
+          "parents": "Strikes",
+          "style_info": ""
+        },
+        {
+          "labels": "Other using Strikes",
+          "locals": 103,
+          "parents": "Strikes",
+          "style_info": ""
+        },
+        {
+          "labels": "Joint lock/Chokeholds/Submisson holds",
+          "locals": 322,
+          "parents": "Grappling",
+          "style_info": ""
+        },
+        {
+          "labels": "Throwing",
+          "locals": 94,
+          "parents": "Grappling",
+          "style_info": ""
+        }
+       ]
--- test_sb_js.js
+++ test_sb_js.js
@@ -1,36 +1,67 @@
+// var data = [
+// {
+//   "type": "sunburst", 
+//   "labels": labels["Strikes", "Punching", "Kicking", "Other Strikes", 
+//             "Grappling", "Throwing", "Joint Lock", "Pinning Tech",
+//             "Boxing", "Wing CHun", "Karate",
+//             "Taekwondo", "Capoeira", "Savate",
+//             "Lethwei", "Muay Thai", "Kung Fu", "Silat", "Kalaripayattu",
+//             "Hapkido", "Judo", "Sumo", "Wrestling", "Aikido",
+//             "Judo","Jujutsu", "Brazillian jiu-jitsu", "Sambo", "Catch Wrestling", 
+//             "Judo", "Wrestling", "Aikido",
+//                                                                     ],
+//   "parents": ["", "Strikes", "Strikes", "Strikes",
+//              "", "Grappling", "Grappling", "Grappling",
+//              "Punching", "Punching", "Punching", 
+//              "Kicking", "Kicking","Kicking", 
+//              "Other Strikes", "Other Strikes","Other Strikes","Other Strikes","Other Strikes",
+//              "Throwing","Throwing","Throwing","Throwing","Throwing",
+//              "Joint Lock","Joint Lock","Joint Lock","Joint Lock","Joint Lock",
+//              "Pinning Tech", "Pinning Tech","Pinning Tech"           
+//             ],
+//   "values":  [10000, 1000, 1000, 1000, 
+//               10000, 1000, 1000, 1000,    
+//               5,5,5,5,
+//               10,10,10,5,
+//               5,5,5,15,
+//               285,223,198,149,
+//               121,119,82,59,
+//               52,44,44,42, ],
+//     "leaf": {"opacity": 0.4},
+//     "marker": {"line": {"width": 2}},
+//     "branchvalues": 'total'
+// }];
+
+// // data2 = sb_data.labels
+// console.log(data2)
+
+var parents = sb_data.map(sb_data => sb_data.parents);
+console.log(parents);
+
+var labels = sb_data.map(sb_data => sb_data.labels);
+console.log(labels);
+
+var values = sb_data.map(sb_data => sb_data.locals);
+console.log(values);
+
+
+
+
 var data = [
 {
-  "type": "sunburst", 
-  "labels": ["Strikes", "Punching", "Kicking", "Other Strikes", 
-            "Grappling", "Throwing", "Joint Lock", "Pinning Tech",
-            "Boxing", "Wing CHun", "Karate",
-            "Taekwondo", "Capoeira", "Savate",
-            "Lethwei", "Muay Thai", "Kung Fu", "Silat", "Kalaripayattu",
-            "Hapkido", "Judo", "Sumo", "Wrestling", "Aikido",
-            "Judo","Jujutsu", "Brazillian jiu-jitsu", "Sambo", "Catch Wrestling", 
-            "Judo", "Wrestling", "Aikido",
-                                                                    ],
-  "parents": ["", "Strikes", "Strikes", "Strikes",
-             "", "Grappling", "Grappling", "Grappling",
-             "Punching", "Punching", "Punching", 
-             "Kicking", "Kicking","Kicking", 
-             "Other Strikes", "Other Strikes","Other Strikes","Other Strikes","Other Strikes",
-             "Throwing","Throwing","Throwing","Throwing","Throwing",
-             "Joint Lock","Joint Lock","Joint Lock","Joint Lock","Joint Lock",
-             "Pinning Tech", "Pinning Tech","Pinning Tech"           
-            ],
-  "values":  [10000, 1000, 1000, 1000, 
-              10000, 1000, 1000, 1000,    
-              5,5,5,5,
-              10,10,10,5,
-              5,5,5,15,
-              285,223,198,149,
-              121,119,82,59,
-              52,44,44,42, ],
-    "leaf": {"opacity": 0.4},
-    "marker": {"line": {"width": 2}},
-    "branchvalues": 'total'
-}];
+  "type":"sunburst",
+  "labels":labels,
+  "parents":parents,
+  "values":values,
+   "leaf": {"opacity": 0.4},
+   "marker": {"line": {"width": 2}},
+   "branchvalues": 'total'
+ }];
+
+
+console.log(sb_data)
+console.log(data)
+
 
 var layout = {
   "margin": {"l": 0, "r": 0, "b": 0, "t": 0},
